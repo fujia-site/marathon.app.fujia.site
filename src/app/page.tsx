e6e956ed8cceb4d02148e4959177e083b081177a
fujia <fujia.site@outlook.com>
2023-02-01 14:04:07 +0800

    feat: add nav

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,23 @@
 // import Image from 'next/image';
 import { Inter } from '@next/font/google';
+import Link from 'next/link';
 import styles from './page.module.css';
 
 const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
-  return <main className={styles.main}>Hello world</main>;
+  return (
+    <main className={styles.main}>
+      <nav>
+        <ol>
+          <li>
+            <Link href="/user">Add User</Link>
+          </li>
+          <li>
+            <Link href="/user/list">User List</Link>
+          </li>
+        </ol>
+      </nav>
+    </main>
+  );
 }
