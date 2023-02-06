7f061f328401e85b1e0348c431ab7edcc9519197
fujia <fujia.site@outlook.com>
2023-02-06 15:07:10 +0800

    feat: remove Inter

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 // import Image from 'next/image';
-import { Inter } from '@next/font/google';
+// import { Inter } from '@next/font/google';
 import Link from 'next/link';
 import styles from './page.module.css';
 
-const inter = Inter({ subsets: ['latin'] });
+// const inter = Inter({ subsets: ['latin'] });
 
 export default function Home() {
   return (
